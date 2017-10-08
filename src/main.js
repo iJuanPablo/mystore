d67d507d113d82bff15ff2f6f789c8825fd30050
Juan Urzua <jp.urzua.t@gmail.com>
2017-10-07 17:44:42 -0700

    vuex store working

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
 import App from './App'
 import router from './router'
+import store from './store'
 
 import firebase from 'firebase'
-
-import Vuex from 'vuex'
-import store from './store'
 
 import VueI18n from 'vue-i18n'
 import ElementUI from 'element-ui'
